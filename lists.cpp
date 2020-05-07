80d9a763ba6341f199dc79109d0039ec20f7cf90
SiddhantDugar <51815274+SiddhantDugar@users.noreply.github.com>
2020-05-07 23:31:13 +0545

    Create lists.cpp

---
+++ lists.cpp
@@ -0,0 +1,49 @@
+//std::list demo
+//only by making few changes in vectors.cpp we can obtain lists.cpp
+
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+void print(const list<int>& my_list)
+{
+    cout <<"List content : {  ";
+    for(const int& num : my_list)
+        cout<<num<<" ";
+    cout<< "}\n\n";
+}
+
+int main()
+{
+    list<int> numbers;
+    int value=0;
+    
+    cout<<"Pushing back...\n";
+    while(value>=0)
+    {
+        cout<<"Enter number: ";
+        cin>>value;
+        if(value>=0)
+            numbers.push_back(value);
+    }
+    
+    print(numbers);
+    
+    value = 0;
+    cout<<"Pushing front...\n";
+    while(value>=0)
+    {
+        cout<<"Enter number: ";
+        cin>>value;
+        if(value>=0)
+            numbers.push_front(value) ;
+    }
+    
+    print(numbers) ;
+    
+    
+    
+    
+    return 0;
+}
